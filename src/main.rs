491fdb2646d61a3207a180b547c489c5886976e3
Jens Östlund <jostlund@gmail.com>
2020-08-18 07:20:37 +0200

    Implement stop timer

--- src/main.rs
+++ src/main.rs
@@ -55,6 +55,10 @@ fn main() {
             _ => {}
         };
 
+        if cpu.registers.st > 0 {
+            cpu.registers.st -= 1;
+        }
+
         thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
 
